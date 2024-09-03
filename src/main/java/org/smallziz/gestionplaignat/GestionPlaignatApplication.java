4e4496fffcf5b8220f1fb977e0d014cd269cec30
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-09-03 10:30:21 +0000

    commit-1

---
+++ src/main/java/org/smallziz/gestionplaignat/GestionPlaignatApplication.java
@@ -0,0 +1,13 @@
+package org.smallziz.gestionplaignat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPlaignatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPlaignatApplication.class, args);
+    }
+
+}
