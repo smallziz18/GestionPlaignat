007ba8a464ccc1305cc70bfcaeb6ad9776a98177
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-09-03 22:28:10 +0000

    je commence a implementer la partie securité

--- src/main/java/org/smallziz/gestionplaignat/model/Plaignant.java
+++ src/main/java/org/smallziz/gestionplaignat/model/Plaignant.java
@@ -4,19 +4,25 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.validator.constraints.UniqueElements;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "plaignant")
-public class Plaignant {
+public class Plaignant implements UserDetails, Principal {
     @Id
     @Column(name = "PLAIGNANT_ID", nullable = false)
     private String plaignantId = UUID.randomUUID().toString();
@@ -61,6 +67,44 @@ public class Plaignant {
     @Size(max = 255)
     @Column(name = "PLAIGNANT_AVATAR")
     private String plaignantAvatar;
+
+    private boolean accountLocked;
+    private String motDePasse;
+
+
+
+    @Override
+    public String getName() {
+        return plaignantPseudo;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return motDePasse;
+    }
+
+    @Override
+    public String getUsername() {
+        return plaignantPseudo;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
     /*@OneToMany(mappedBy = "plaignant")
     private Set<Complainte> complaintes = new LinkedHashSet<>();*/
